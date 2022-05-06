18604e317d39d67e11bf09a1604d06263b663cc4
Henrique Cecconi <henrique.cecconi@gmail.com>
2022-05-05 22:57:46 -0300

    Created a basic simulation of a bank account in the class Account. Also tried some things with the list of those accounts.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python Collections
+
+This is a study repo, made so I can play and get more confortable with python collections 
---
+++ account.py
@@ -0,0 +1,25 @@
+class Account():
+    
+    def __init__(self, account_number):
+        self.account_number = account_number
+        self.balance = 0
+    
+    def __str__(self):
+        return f'>>Account Number: {self.account_number}, Balance: {self.balance}<<'
+        
+    def deposit(self, value):
+        self.balance += value
+
+
+harry_account = Account(1234)
+leah_account = Account(4321)
+harry_account.deposit(100)
+leah_account.deposit(1000)
+accounts = [harry_account, leah_account]
+
+print(accounts) #This does not call the __str__ method inside the Class, since I'm printing the list as a whole
+#Printing the accounts individually so python access the __str__ method
+for account in accounts:
+    print(account)
+#Writing the same thing as above, but with list comprehension
+[print(account) for account in accounts]
